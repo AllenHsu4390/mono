0a3735ad6e3dc7423f0d8e0f719729e0765c1552
Allen Hsu <ahsu@atlassian.com>
2022-02-02 15:53:12 -0800

    add mui

--- mono-server/pages/index.tsx
+++ mono-server/pages/index.tsx
@@ -1,29 +1,8 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Page from "./ui/page";
-import styles from "../styles/Home.module.css";
+import Album from './ui/album';
 
 const Home: NextPage = () => {
   return (
-    <Page
-      title={<>Mono server</>}
-      main={
-        <>
-          <h1 className={styles.title}>Projects</h1>
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div className={styles.grid}>
-            <a href="/genshin" className={styles.card}>
-              <h2>Project A &rarr;</h2>
-              <p>genshin calculator</p>
-            </a>
-          </div>
-        </>
-      }
-    />
+    <Album />
   );
 };
 
