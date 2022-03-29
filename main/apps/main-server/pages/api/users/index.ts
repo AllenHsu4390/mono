c5f18535964c7c64f96d43079cc00aa69646f780
DESKTOP-FTAFV3J\allen <allenhsu4390@gmail.com>
2022-03-28 23:39:52 -0700

    add auth

--- main/apps/main-server/pages/api/users/index.ts
+++ main/apps/main-server/pages/api/users/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Users } from '@main/models';
 import { Error } from '@main/models';
+import { auth } from '@main/auth';
 import { environment } from '@main/environment';
 
 export default async function handler(
@@ -8,5 +9,6 @@ export default async function handler(
   res: NextApiResponse<Users | Error>
 ) {
   const db = environment().db;
-  res.status(200).json(await db.get.users('0'));
+  const userId = auth().identity.userId('asdfasdfasdfasdf');
+  res.status(200).json(await db.get.users(userId));
 }
