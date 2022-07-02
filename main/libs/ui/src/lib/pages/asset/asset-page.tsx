d15835f8553df71a8aa05f24866a64e238583278
DESKTOP-FTAFV3J\allen <allenhsu4390@gmail.com>
2022-07-02 00:13:01 -0700

    add asset actions bar

--- main/libs/ui/src/lib/pages/asset/asset-page.tsx
+++ main/libs/ui/src/lib/pages/asset/asset-page.tsx
@@ -1,9 +1,8 @@
 import Page from '../_base/page';
 import AssetInteractable from '../../block/asset/asset';
 import type { AssetResponse } from '@main/rest-models';
-import { Container, Stack, Switch, Typography } from '@mui/material';
-import DeleteAssetButton from '../../element/delete-asset-button';
-import { VisibilityButton } from '../../element/visibility-button';
+import { Container } from '@mui/material';
+import { AssetActionsBar } from '../../block/asset-actions-bar';
 
 export interface AssetPageProps {
   asset: AssetResponse;
@@ -13,20 +12,7 @@ export const AssetPage = ({ asset }: AssetPageProps) => {
   return (
     <Page
       title={asset.creator.name}
-      actionsBar={
-        asset.links.delete ? (
-          <Stack
-            direction="row-reverse"
-            spacing={'1rem'}
-            sx={{
-              pr: '1rem',
-            }}
-          >
-            {asset.links.delete ? <DeleteAssetButton asset={asset} /> : null}
-            {asset.links.delete ? <VisibilityButton asset={asset} /> : null}
-          </Stack>
-        ) : null
-      }
+      actionsBar={asset.links.delete ? <AssetActionsBar asset={asset} /> : null}
     >
       <Container>
         <AssetInteractable asset={asset} />
